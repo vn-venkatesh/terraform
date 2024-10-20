ab06f093a49919da23938077d5babf4294817238
vn-venkatesh <110007407+vn-venkatesh@users.noreply.github.com>
2024-10-20 18:31:53 +0530

    Delete modules/modules/compute_instance directory

--- modules/modules/compute_instance/output.tf
+++
@@ -1,4 +0,0 @@
-output "instance_external_ip" {
-  description = "The external IP address of the instance"
-  value       = google_compute_instance.default.network_interface[0].access_config[0].nat_ip
-}
